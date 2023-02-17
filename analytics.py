2961fd1c1073c4d9171a05d8f181f1fa95eaa8af
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-02-17 15:11:50 +0100

    fichier analytique

---
+++ analytics.py
@@ -0,0 +1,139 @@
+import csv
+import os.path
+
+import matplotlib.pyplot as plot
+
+age_index = 0
+interested_index = 1
+known_qr_index = 3
+has_smartphone_index = 4
+where_to_buy_index = 5
+price_index = 6
+plants_wanted_index = 7
+email_index = 10
+name_index = 11
+date_index = 12
+
+
+def get_average_age(rows):
+    ages = []
+    for row in rows:
+        ages.append(int(row[age_index]))
+    return sum(ages) / len(ages)
+
+
+def get_prices_by_percent(rows):
+    prices = [row[price_index] for row in rows]
+    total = len(prices)
+    prices_by_number = {}
+    prices_by_percent = {}
+    for price in prices:
+        if prices_by_number.get(price) is None:
+            prices_by_number[price] = 1
+            continue
+        prices_by_number[price] += 1
+    for price in prices_by_number.keys():
+        prices_by_percent[price] = 100 * prices_by_number[price] / total
+    return prices_by_percent
+
+
+def get_places_by_percent(rows):
+    places = [place for row in rows for place in row[where_to_buy_index].split(', ')]
+    total = len(places)
+    places_by_number = {}
+    places_by_percent = {}
+    for place in places:
+        if places_by_number.get(place) is None:
+            places_by_number[place] = 1
+            continue
+        places_by_number[place] += 1
+    for place in places_by_number.keys():
+        places_by_percent[place] = 100 * places_by_number[place] / total
+    return places_by_percent
+
+
+def get_wanted_plants_by_percent(rows):
+    plants = [plant for row in rows for plant in row[plants_wanted_index].split(', ')]
+    total = len(plants)
+    plants_by_number = {}
+    plants_by_percent = {}
+    for plant in plants:
+        if plants_by_number.get(plant) is None:
+            plants_by_number[plant] = 1
+            continue
+        plants_by_number[plant] += 1
+    for plant in plants_by_number.keys():
+        plants_by_percent[plant] = 100 * plants_by_number[plant] / total
+    return plants_by_percent
+
+
+def get_percentage_of_persons_who_know_qr(rows):
+    number_of_person_who_know = 0
+    for row in rows:
+        if row[known_qr_index] == 'Oui':
+            number_of_person_who_know += 1
+    percentage = 100 * number_of_person_who_know / len(rows)
+    stay = 100 - percentage
+    return {'Oui': percentage, 'Non': stay}
+
+
+def get_emails_names(rows):
+    emails_names_list = []
+    for row in rows:
+        if row[email_index] != '' or row[name_index] != '':
+            emails_names_list.append((row[email_index], row[name_index]))
+    return emails_names_list
+
+
+def generate_pie_graph(datas, labels, name):
+    explode = [0 for _ in datas]
+    explode[datas.index(max(datas))] = 0.2
+    plot.figure(figsize=(16, 9))
+    plot.pie(datas, labels=labels,
+             autopct=lambda x: str(round(x, 2)) + '%',
+             pctdistance=0.7,
+             labeldistance=1.4,
+             explode=explode,
+             shadow=True)
+    plot.legend()
+    plot.savefig(f'graphs/{name}.png')
+
+
+if __name__ == '__main__':
+    with open('tableau-resultats-etude-de-marche.csv') as file:
+        rows = list(csv.reader(file))[1:-1]
+        if not os.path.isdir('graphs'):
+            os.mkdir('graphs')
+        print('Planteqr - Étude de marché analytiques:')
+        graphs = True if input('Générer des graphiques ?[y/n]') == 'y' else 'n'
+        print(f"{len(rows)} entrées:")
+
+        average_age = get_average_age(rows)
+
+        prices_by_percent = get_prices_by_percent(rows)
+        prices_by_percent_string = ''.join([f'\n\t- {price}: {value}%' for price, value in prices_by_percent.items()])
+
+        places_by_percent = get_places_by_percent(rows)
+        places_by_percent_string = ''.join([f'\n\t- {place}: {value}%' for place, value in places_by_percent.items()])
+
+        wanted_plants_by_percent = get_wanted_plants_by_percent(rows)
+        wanted_plants_by_percent_string = ''.join([f'\n\t- {plant}: {value}%' for plant, value in wanted_plants_by_percent.items()])
+
+        percentage_of_persons_who_know_qr = get_percentage_of_persons_who_know_qr(rows)
+        percentage_of_persons_who_know_qr_string = f"Oui: {percentage_of_persons_who_know_qr['Oui']}%, Non: {percentage_of_persons_who_know_qr['Non']}%"
+
+        emails_names = get_emails_names(rows)
+        emails_names_string = ''.join([f'\n\t - ' + email + ' : ' + nom for email, nom in emails_names])
+
+        print(f"Moyenne d'âge: {average_age} ans")
+        print(f"Taux/prix voulu: {prices_by_percent_string}")
+        print(f"Taux/lieu de vente voulu: {places_by_percent_string}")
+        print(f"Taux/type de plante voulu: {wanted_plants_by_percent_string}")
+        print(f"Connaissent le QR code: {percentage_of_persons_who_know_qr_string}")
+        print(f"Email / noms récupérés: {emails_names_string}")
+
+        if graphs:
+            generate_pie_graph([prices_by_percent[key] for key in prices_by_percent.keys()], prices_by_percent.keys(), 'prix-de-vente')
+            generate_pie_graph([places_by_percent[key] for key in places_by_percent.keys()], places_by_percent.keys(), 'lieux-de-vente')
+            generate_pie_graph([wanted_plants_by_percent[key] for key in wanted_plants_by_percent.keys()], wanted_plants_by_percent.keys(), 'types-de-plante')
+            generate_pie_graph([percentage_of_persons_who_know_qr[key] for key in percentage_of_persons_who_know_qr.keys()], percentage_of_persons_who_know_qr.keys(), 'connaissent-qr')
